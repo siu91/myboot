706785867c53db0602f59d1821666bbff785886f
siu <gongshiwen@gmail.com>
2020-02-20 20:42:44 +0800

    refactor(myboot-core): 数据相关模块移到myboot-core-data

---
+++ myboot-core/myboot-core-data/myboot-core-data-multids/src/main/java/org/siu/myboot/core/data/config/JdbcTemplateConfig.java
@@ -0,0 +1,25 @@
+package org.siu.myboot.core.data.config;
+
+/**
+ * JdbcTemplate 多数据源配置
+ *
+ * @Author Siu
+ * @Date 2020/2/17 12:29
+ * @Version 0.0.1
+ */
+/*@Configuration
+public class JdbcTemplateConfig {
+
+
+    @Bean(name = "primaryJdbcTemplate")
+    public JdbcTemplate primaryJdbcTemplate(
+            @Qualifier("primaryDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean(name = "secondaryJdbcTemplate")
+    public JdbcTemplate secondaryJdbcTemplate(
+            @Qualifier("secondaryDataSource") DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}*/
---
+++ myboot-core/myboot-core-data/myboot-core-data-multimbss/src/main/java/org/siu/myboot/core/data/config/PrimarySqlSessionConfig.java
@@ -0,0 +1,41 @@
+package org.siu.myboot.core.data.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+/**
+ * mybatis多数据源配置：SqlSession 配置 （primary）
+ *
+ * @Author Siu
+ * @Date 2020/2/18 13:47
+ * @Version 0.0.1
+ */
+@Configuration
+@MapperScan(basePackages = "org.siu.**.mapper.primary", sqlSessionTemplateRef  = "primarySqlSessionTemplate")
+public class PrimarySqlSessionConfig {
+
+    @Bean(name = "primarySqlSessionFactory")
+    @Primary
+    public SqlSessionFactory buildSqlSessionFactory(@Qualifier("primaryDataSource") DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mybatis/mapper/primary/*.xml"));
+        return bean.getObject();
+    }
+
+
+    @Bean(name = "primarySqlSessionTemplate")
+    @Primary
+    public SqlSessionTemplate buildSqlSessionTemplate(@Qualifier("primarySqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
