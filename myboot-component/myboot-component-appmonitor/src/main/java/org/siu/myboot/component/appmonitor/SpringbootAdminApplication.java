6dfabcf20baec25bdce4eea19f201cce64dadffd
siu <gongshiwen@gmail.com>
2020-02-22 10:02:28 +0800

    feat: spring boot admin server

---
+++ myboot-component/myboot-component-appmonitor/src/main/java/org/siu/myboot/component/appmonitor/SpringbootAdminApplication.java
@@ -0,0 +1,19 @@
+package org.siu.myboot.component.appmonitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Siu
+ * @Date 2020/2/22 9:49
+ * @Version 0.0.1
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class SpringbootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAdminApplication.class, args);
+    }
+}
+
