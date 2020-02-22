d3442f9cc75e60127a6012e3b626bbce3465d82d
siu <gongshiwen@gmail.com>
2020-02-22 17:40:40 +0800

    feat:整合jpa + queryDSL + p6spy

---
+++ myboot-core/myboot-core-data/myboot-core-data-querydsljpa/src/main/java/org/siu/myboot/core/data/p6spy/P6SpyLogger.java
@@ -0,0 +1,22 @@
+package org.siu.myboot.core.data.p6spy;
+
+import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
+
+import java.time.LocalDateTime;
+
+/**
+ * 自定义p6spy日志格式
+ *
+ * @Author Siu
+ * @Date 2020/2/22 16:46
+ * @Version 0.0.1
+ */
+public class P6SpyLogger implements MessageFormattingStrategy {
+
+    @Override
+    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql) {
+        return !"".equals(sql.trim()) ? "[ " + LocalDateTime.now() + " ] --- | took "
+                + elapsed + "ms | " + category + " | connection " + connectionId + "\n "
+                + sql + ";" : "";
+    }
+}
---
+++ myboot-core/myboot-core-data/myboot-core-data-querydsljpa/src/main/java/org/siu/myboot/core/data/querydsljpa/BaseJpaRepository.java
@@ -0,0 +1,52 @@
+package org.siu.myboot.core.data.querydsljpa;
+
+import com.querydsl.core.types.EntityPath;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.JpaEntityInformationSupport;
+import org.springframework.data.jpa.repository.support.Querydsl;
+import org.springframework.data.jpa.repository.support.QuerydslJpaPredicateExecutor;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.data.querydsl.SimpleEntityPathResolver;
+import org.springframework.data.repository.support.PageableExecutionUtils;
+
+import javax.persistence.EntityManager;
+
+/**
+ * BaseJpaRepository （JPA + QueryDSL）
+ *
+ * @Author Siu
+ * @Date 2020/2/22 16:48
+ * @Version 0.0.1
+ */
+public class BaseJpaRepository<T, ID> extends SimpleJpaRepository<T, ID> {
+
+    protected final JPAQueryFactory jpaQueryFactory;
+    protected final QuerydslJpaPredicateExecutor<T> jpaPredicateExecutor;
+    protected final EntityManager em;
+    private final EntityPath<T> path;
+    protected final Querydsl querydsl;
+
+    BaseJpaRepository(Class<T> domainClass, EntityManager em) {
+        super(domainClass, em);
+        this.em = em;
+        this.jpaPredicateExecutor = new QuerydslJpaPredicateExecutor<>(JpaEntityInformationSupport.getEntityInformation(domainClass, em), em, SimpleEntityPathResolver.INSTANCE, getRepositoryMethodMetadata());
+        this.jpaQueryFactory = new JPAQueryFactory(em);
+        this.path = SimpleEntityPathResolver.INSTANCE.createPath(domainClass);
+        this.querydsl = new Querydsl(em, new PathBuilder<T>(path.getType(), path.getMetadata()));
+    }
+
+    protected Page<T> findAll(Predicate predicate, Pageable pageable, OrderSpecifier<?>... orders) {
+        final JPAQuery countQuery = jpaQueryFactory.selectFrom(path);
+        countQuery.where(predicate);
+        JPQLQuery<T> query = querydsl.applyPagination(pageable, countQuery);
+        query.orderBy(orders);
+        return PageableExecutionUtils.getPage(query.fetch(), pageable, countQuery::fetchCount);
+    }
+}
