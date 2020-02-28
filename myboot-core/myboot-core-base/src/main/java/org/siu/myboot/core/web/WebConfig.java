52e679377b9debc0983048c8eae9b0958bcc0ddb
siu <gongshiwen@gmail.com>
2020-02-28 16:09:39 +0800

    feat:fastjson 替换jackson

--- myboot-core/myboot-core-base/src/main/java/org/siu/myboot/core/web/WebConfig.java
+++ myboot-core/myboot-core-base/src/main/java/org/siu/myboot/core/web/WebConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.validation.MessageCodesResolver;
 import org.springframework.validation.Validator;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -141,6 +142,49 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> list) {
+         /*
+
+    先把JackSon的消息转换器删除.
+    备注: (1)源码分析可知，返回json的过程为:
+               Controller调用结束后返回一个数据对象，for循环遍历conventers，找到支持application/json的HttpMessageConverter，然后将返回的数据序列化成json。
+               具体参考org.springframework.web.servlet.mvc.method.annotation.AbstractMessageConverterMethodProcessor的writeWithMessageConverters方法
+          (2)由于是list结构，我们添加的fastjson在最后。因此必须要将jackson的转换器删除，不然会先匹配上jackson，导致没使用fastjson
+
+   */
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (list.get(i) instanceof MappingJackson2HttpMessageConverter) {
+                list.remove(i);
+            }
+        }
+
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+
+        //自定义fastjson配置
+        FastJsonConfig config = new FastJsonConfig();
+        config.setSerializerFeatures(
+                // 是否输出值为null的字段,默认为false,我们将它打开
+                SerializerFeature.WriteMapNullValue,
+                // 将Collection类型字段的字段空值输出为[]
+                SerializerFeature.WriteNullListAsEmpty,
+                // 将字符串类型字段的空值输出为空字符串
+                SerializerFeature.WriteNullStringAsEmpty,
+                // 将数值类型字段的空值输出为0
+                SerializerFeature.WriteNullNumberAsZero,
+                SerializerFeature.WriteDateUseDateFormat,
+                // 禁用循环引用
+                SerializerFeature.DisableCircularReferenceDetect
+
+        );
+
+        fastJsonHttpMessageConverter.setFastJsonConfig(config);
+
+        // 添加支持的MediaTypes;不添加时默认为*/*,也就是默认支持全部
+        // 但是MappingJackson2HttpMessageConverter里面支持的MediaTypes为application/json
+        // 参考它的做法, fastjson也只添加application/json的MediaType
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastJsonHttpMessageConverter.setSupportedMediaTypes(fastMediaTypes);
+        list.add(fastJsonHttpMessageConverter);
 
     }
 
@@ -153,23 +197,6 @@ public class WebConfig implements WebMvcConfigurer {
      */
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> list) {
-        /** 1.需要定义一个convert转换消息的对象;*/
-        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
-        /** 2.添加fastJson的配置信息，比如：是否要格式化返回的json数据;*/
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat,
-                SerializerFeature.WriteMapNullValue,
-                SerializerFeature.WriteNullStringAsEmpty,
-                SerializerFeature.DisableCircularReferenceDetect,
-                SerializerFeature.WriteNullListAsEmpty);
-        /** 3处理中文乱码问题*/
-        List<MediaType> fastMediaTypes = new ArrayList<>();
-        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-        /** 4.在convert中添加配置信息.*/
-        fastJsonHttpMessageConverter.setSupportedMediaTypes(fastMediaTypes);
-        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
-        /** 5.将convert添加到converters当中.*/
-        list.add(fastJsonHttpMessageConverter);
     }
 
     @Override
