2a07f15ca78197ced2a360942a27e58b73a53289
siu <gongshiwen@gmail.com>
2020-03-19 23:34:26 +0800

    feat:通用动态数据源模块

---
+++ myboot-framework/myboot-core/myboot-core-dds/src/main/java/org/siu/myboot/datasource/context/DynamicDataSourceContextHolder.java
@@ -0,0 +1,80 @@
+package org.siu.myboot.datasource.context;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.core.NamedInheritableThreadLocal;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 数据源切换
+ * <p>
+ * 基于ThreadLocal
+ * NamedInheritableThreadLocal（可以被子线程继承的）
+ *
+ * @Author Siu
+ * @Date 2020/3/18 22:56
+ * @Version 0.0.1
+ */
+@UtilityClass
+public class DynamicDataSourceContextHolder {
+
+    /**
+     * 为什么要用链表存储(准确的是栈)
+     * <pre>
+     * 为了支持嵌套切换，如ABC三个service都是不同的数据源
+     * 其中A的某个业务要调B的方法，B的方法需要调用C的方法。一级一级调用切换，形成了链。
+     * 传统的只设置当前线程的方式不能满足此业务需求，必须模拟栈，后进先出。
+     * </pre>
+     */
+    private static final ThreadLocal<Deque<String>> CURRENT_DATASOURCE_KEY_HOLDER = new NamedInheritableThreadLocal<Deque<String>>("DynamicDataSourceContextHolder") {
+        @Override
+        protected Deque<String> initialValue() {
+            return new ArrayDeque<>();
+        }
+    };
+
+    /**
+     * 获取当前线程数据源
+     *
+     * @return 数据源名称
+     */
+    public static String peek() {
+        return CURRENT_DATASOURCE_KEY_HOLDER.get().peek();
+    }
+
+    /**
+     * 设置当前线程数据源
+     *
+     * @param ds 数据源名称
+     */
+    public static void push(String ds) {
+        CURRENT_DATASOURCE_KEY_HOLDER.get().push(StringUtils.isEmpty(ds) ? "" : ds);
+    }
+
+    /**
+     * 清空当前线程数据源
+     * <p>
+     * 如果当前线程是连续切换数据源 只会移除掉当前线程的数据源名称
+     * </p>
+     */
+    public static void poll() {
+        Deque<String> deque = CURRENT_DATASOURCE_KEY_HOLDER.get();
+        deque.poll();
+        if (deque.isEmpty()) {
+            CURRENT_DATASOURCE_KEY_HOLDER.remove();
+        }
+    }
+
+    /**
+     * 强制清空本地线程
+     * <p>
+     * 防止内存泄漏，如手动调用了push可调用此方法确保清除
+     * </p>
+     */
+    public static void clear() {
+        CURRENT_DATASOURCE_KEY_HOLDER.remove();
+    }
+
+}
